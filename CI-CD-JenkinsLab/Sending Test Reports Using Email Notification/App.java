3ed05160ab8e8174f8ad8cb59ad3d4bd8ec7bdbe
learnerrv <149032678+learnerrv@users.noreply.github.com>
2025-08-25 11:40:09 +0530

    Create App.java

---
+++ CI-CD-JenkinsLab/Sending Test Reports Using Email Notification/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+}
